b6ca89d758c7f5351011c17593edab4fc98848b6
sajjadahmed677 <merledu677@gmail.com>
2020-12-19 16:09:13 +0500

    GPIO write test updated

--- gpio tests/direct_write_gpio.asm
+++ gpio tests/direct_write_gpio.asm
@@ -1,11 +1,24 @@
+trap_handler_start:
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+jal trap_handler_start
 li s0,0x40010000
-li x10,8
-li x11,8
+li x10,4
+li x11,4
 sw x10,0x1c(s0)
 sw x11,0x10(s0)
 
 // test status//
 //passed
-//this test will directly write 1 to 4th gpio pin
+//this test will directly write 1 to 3rd gpio pin
 
 //machine code is available in direct_write_gpio.mach
