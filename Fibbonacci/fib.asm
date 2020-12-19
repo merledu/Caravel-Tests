9a0038e720c995a8a22160742a0e93611f7fc837
sajjadahmed677 <merledu677@gmail.com>
2020-12-19 18:18:52 +0500

    fabonacii test updated

--- Fibbonacci/fib.asm
+++ Fibbonacci/fib.asm
@@ -1,33 +1,32 @@
-main:
-        addi    sp,sp,-48
-        sw      s0,44(sp)
-        addi    s0,sp,48
-        sw      zero,-24(s0)
-        li      a5,1
-        sw      a5,-28(s0)
-        li      a5,5
-        sw      a5,-32(s0)
-        li      a5,1
-        sw      a5,-20(s0)
-.L3:
-        lw      a4,-20(s0)
-        lw      a5,-36(s0)
-        bgt     a4,a5,.L2
-        lw      a4,-24(s0)
-        lw      a5,-28(s0)
-        add     a5,a4,a5
-        sw      a5,-32(s0)
-        lw      a5,-28(s0)
-        sw      a5,-24(s0)
-        lw      a5,-32(s0)
-        sw      a5,-28(s0)
-        lw      a5,-20(s0)
-        addi    a5,a5,1
-        sw      a5,-20(s0)
-        j       .L3
-.L2:
-        li      a5,0
-        mv      a0,a5
-        lw      s0,44(sp)
-        addi    sp,sp,48
-        jr      ra
+start_trap_handler:
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+jal start_trap_handler
+li s0, 0x40010000
+li x10,1 # i
+li x11,6 # n
+li x12,0 # t1
+li x13,1 # t2
+li x14,0 # result
+loop:
+beq x10,x11,end
+add x14,x13,x12
+sw x14,0x1c(s0)
+sw x14,0x10(s0)
+mv x12,x13
+mv x13,x14
+addi x10,x10,1
+jal loop
+end:
+
+// status passed//
+// test will write each fabonacii resultant term on GPIOs.
