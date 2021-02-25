5083ddb307ddddf491e8ed7c0289fad514ca883c
Hadir Khan <hadirkhan10@gmail.com>
2021-02-25 10:26:40 +0500

    Modified the mean program

--- Pre_Layout_Verification_test/mean/mean.s
+++ Pre_Layout_Verification_test/mean/mean.s
@@ -1,26 +1,26 @@
-li x15, 0x40010000 	# gpio base address
-li x14, 0x10000000 	# dccm base address for ibtida  # for ghazi change it to 0x18000000
-addi x16, x0, 4		# first number
-addi x17, x0, 6		# second number
-addi x18, x0, 8		# third number
-sw x16, 0(x14)		# storing numbers in dccm
-sw x17, 4(x14)		# storing numbers in dccm
-sw x18, 8(x14)		# storing numbers in dccm
+li x8, 0x40010000 	# gpio base address
+li x9, 0x10000000 	# dccm base address for ibtida  # for ghazi change it to 0x18000000
+addi x16, x0, 6		# first number
+addi x17, x0, 8		# second number
+addi x18, x0, 10		# third number
+sw x16, 0(x9)		# storing numbers in dccm
+sw x17, 4(x9)		# storing numbers in dccm
+sw x18, 8(x9)		# storing numbers in dccm
 jal average
-lw x24, 0xc(x14)
-li x25, 0xff
-sw x25, 0x1c(x15)
-sw x24, 0x10(x15)	# expected output in gpio is 6
+lw x7, 0xc(x9)
+li x6, 0xff
+sw x6, 0x1c(x8)
+sw x7, 0x10(x8)	# expected output in gpio is 8
 jal end
 average:
-	lw x19, 0(x14)
-    lw x20, 4(x14)
-    lw x21, 8(x14)
-    add x20, x19, x20
-    add x21, x20, x21
-    li x22, 3
-    div x23, x21, x22
-    sw x23, 0xc(x14)	#saving the average
+	lw x14, 0(x9)
+    lw x13, 4(x9)
+    lw x12, 8(x9)
+    add x13, x14, x13
+    add x12, x13, x12
+    li x11, 3
+    div x10, x12, x11
+    sw x10, 0xc(x9)	#saving the average
     ret
 end:
-
+jal end
