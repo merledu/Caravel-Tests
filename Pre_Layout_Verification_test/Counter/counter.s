42f440eea9943d38076956a82c3cd50c52ae5bef
Hadir Khan <hadirkhan10@gmail.com>
2021-02-25 11:50:41 +0500

    modified the counter program

--- Pre_Layout_Verification_test/Counter/counter.s
+++ Pre_Layout_Verification_test/Counter/counter.s
@@ -1,5 +1,5 @@
 li x8, 0x40010000 # gpio address
-li x9, 0x18000000 # DCCM address for ghazi # for Ibtida change it to 0x10000000
+li x9, 0x10000000 # DCCM address for ghazi # for Ibtida change it to 0x10000000
 li x7, 0xff
 sw x7, 0x1c(x8)
 
@@ -12,11 +12,12 @@ loop:
 lw x7, 0x4(x9)
 addi x7, x7, 1
 sw x7, 0x10(x8)
-beq x7, x6, loop1
+beq x7, x6, write_gpio
 sw x7, 0x4(x9)
 j loop
 
-loop1:
+write_gpio:
 sw x7, 0x10(x8)
-li x7, 0
-sw x7, 0x10(x8)
+
+end:
+jal end
