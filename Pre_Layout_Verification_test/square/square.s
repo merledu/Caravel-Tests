eb2bf5b85867c69154a2a55bb218ba3b1cc610a1
Hadir Khan <hadirkhan10@gmail.com>
2021-02-25 10:00:28 +0500

    modified the square program

--- Pre_Layout_Verification_test/square/square.s
+++ Pre_Layout_Verification_test/square/square.s
@@ -1,16 +1,17 @@
-li x20, 0x40010000		# gpio base address
-li x21, 0x10000000		# dccm base address for ibtida # for ghazi change it to 0x18000000
+li x8, 0x40010000		# gpio base address
+li x9, 0x10000000		# dccm base address
 addi x15, x0, 4			# any number that needs to be squared
-sw x15, 0(x21)
+sw x15, 0(x9)
 jal square
-lw x17, 4(x21)
-addi x18, x0, 0x000000FF
-sw x18, 0x1c(x20)
-sw x17, 0x10(x20)
+lw x17, 4(x9)
+li x18, 0xff
+sw x18, 0x1c(x8)
+sw x17, 0x10(x8)
 jal end
 square:
-	lw x16, 0(x21)
+	lw x16, 0(x9)
     mul x16, x16, x16
-    sw x16, 4(x21)
+    sw x16, 4(x9)
     ret
 end:
+jal end
