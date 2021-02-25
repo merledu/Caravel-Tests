4bba9f0e3c5cad59b1cc7c7a90324d4b4a675454
Hadir Khan <hadirkhan10@gmail.com>
2021-02-25 10:43:52 +0500

    modified the factorial program

--- Pre_Layout_Verification_test/factorial/factorial.s
+++ Pre_Layout_Verification_test/factorial/factorial.s
@@ -1,16 +1,15 @@
 li s0, 0x40010000  # gpio base address
-li x10,6
+li x10,0xff
 li x11,1
 li x12,1
-li x13,4
+li x13,4			# the number whose factorial needs to be calculated
 loop:
-bgt x12,x13,end
+bgt x12,x13,gpio_write
 mul x11,x11,x12
 addi x12,x12,1
 jal loop
+gpio_write:
+sw x10,0x1c(s0)		
+sw x11, 0x10(s0)	# should write 4! = 24 on GPIO
 end:
-sw x10,0x1c(s0)
-sw x11, 0x10(s0)
-
-
-//test will write factorial of 4 on gpio in this case the answer is 24 so it will write value 18 on gpio block.
+jal end
